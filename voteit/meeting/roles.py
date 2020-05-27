ec5b3bc5f687b35c9f7bafced6125460d030d460
Robin Harms Oredsson <robin@betahaus.net>
2020-05-27 22:07:48 +0200

    First stab at generic role assignments. Related to #1

---
+++ voteit/meeting/roles.py
@@ -0,0 +1,10 @@
+from voteit.core.role import Role, roles
+from voteit.meeting.models import Meeting
+from voteit.meeting.rules import is_participant
+
+
+@roles
+class Participant(Role):
+    rule = is_participant
+    model = Meeting
+    m2m_field = "participants"
