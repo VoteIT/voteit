c50c20b6d4b8ed426c55750318e5f18a90798772
Robin Harms Oredsson <robin@betahaus.net>
2020-05-28 10:46:38 +0200

    Forgot title

--- voteit/meeting/roles.py
+++ voteit/meeting/roles.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 from voteit.core.role import Role, roles
 from voteit.meeting.models import Meeting
 from voteit.meeting.rules import is_participant
@@ -8,3 +10,4 @@ class Participant(Role):
     rule = is_participant
     model = Meeting
     m2m_field = "participants"
+    title = _("Meeting participant")
