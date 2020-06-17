f4f1ef6459c62339a22d17fccfce8d5dcd15e02d
Robin Harms Oredsson <robin@betahaus.net>
2020-06-17 14:44:39 +0200

    Permissions for discussions

--- voteit/discussion/rules.py
+++ voteit/discussion/rules.py
@@ -4,7 +4,13 @@ from voteit.agenda.models import AgendaItem
 from voteit.agenda.workflows import AgendaItemWf
 from voteit.core.rules import is_author
 from voteit.discussion.models import DiscussionPost
-from voteit.meeting.rules import is_moderator, is_not_archived, is_discusser, is_participant
+from voteit.meeting.rules import (
+    is_moderator,
+    is_not_archived,
+    is_discusser,
+    is_participant,
+    is_public,
+)
 from voteit.discussion.permissions import DiscussionPermissions
 
 
@@ -24,10 +30,10 @@ def can_add_discussion_post(user: User, agenda_item: AgendaItem):
     if isinstance(agenda_item, AgendaItem) and is_not_archived(
         user, agenda_item.meeting
     ):
-        return is_moderator(agenda_item.meeting) or (
+        return is_moderator(user, agenda_item.meeting) or (
             is_non_private_ai(user, agenda_item)
             and is_not_discussion_blocked(user, agenda_item)
-            and is_discusser(agenda_item.meeting)
+            and is_discusser(user, agenda_item.meeting)
         )
 
 
@@ -36,9 +42,13 @@ def can_view_discussion_post(user: User, discussion_post: DiscussionPost):
     """
     try:
         meeting = discussion_post.agenda_item.meeting  # Will catch None too
-    except AttributeError:
+    except AttributeError:  # pragma: no cover
         return
-    return is_moderator(user, meeting) or is_non_private_ai(user, discussion_post.agenda_item) and is_participant(user, meeting)
+    return (
+        is_moderator(user, meeting)
+        or is_non_private_ai(user, discussion_post.agenda_item)
+        and (is_participant(user, meeting) or is_public(user, meeting))
+    )
 
 
 def can_change_discussion_post(user: User, discussion_post: DiscussionPost):
@@ -47,7 +57,7 @@ def can_change_discussion_post(user: User, discussion_post: DiscussionPost):
     # FIXME: Do we want versioning and allow changes here?
     try:
         meeting = discussion_post.agenda_item.meeting  # Will catch None too
-    except AttributeError:
+    except AttributeError:  # pragma: no coverage
         return
     return is_not_archived(user, meeting) and is_moderator(user, meeting)
 
@@ -55,7 +65,7 @@ def can_change_discussion_post(user: User, discussion_post: DiscussionPost):
 def can_delete_discussion_post(user: User, discussion_post: DiscussionPost):
     try:
         meeting = discussion_post.agenda_item.meeting  # Will catch None too
-    except AttributeError:
+    except AttributeError:  # pragma: no coverage
         return
     if is_not_archived(user, meeting):
         return is_moderator(user, meeting) or is_author(user, discussion_post)
@@ -65,4 +75,3 @@ rules.add_perm(DiscussionPermissions.ADD, can_add_discussion_post)
 rules.add_perm(DiscussionPermissions.VIEW, can_view_discussion_post)
 rules.add_perm(DiscussionPermissions.CHANGE, can_change_discussion_post)
 rules.add_perm(DiscussionPermissions.DELETE, can_delete_discussion_post)
-
---
+++ voteit/discussion/tests/test_rules.py
@@ -0,0 +1,134 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+class RulesTests(TestCase):
+    def setUp(self):
+        from voteit.meeting.models import Meeting
+        from voteit.meeting.roles import Moderator
+        from voteit.meeting.roles import Discusser
+
+        self.meeting = Meeting.objects.create()
+        self.anon_user = User.objects.create(username="anon")
+        self.participant = self.meeting.participants.create(username="participant")
+        self.moderator = self.meeting.moderators.create(username="moderator")
+        Moderator(self.meeting).add(self.moderator)
+        self.discusser = self.meeting.discussers.create(username="discusser")
+        self.discusser_author = self.meeting.discussers.create(
+            username="discusser_author"
+        )
+        Discusser(self.meeting).add(self.discusser, self.discusser_author)
+        self.ai = self.meeting.agenda_items.create()
+        self.ai.upcoming()
+        self.ai.save()
+        self.discussion_post = self.ai.discussions.create(author=self.discusser_author)
+
+    def p(self, perm):
+        from voteit.discussion.permissions import DiscussionPermissions
+
+        return getattr(DiscussionPermissions, perm)
+
+    def _archive(self):
+        self.meeting.ongoing()
+        self.meeting.closed()
+        self.meeting.archive()
+        self.meeting.save()
+
+    def test_view_private(self):
+        self.ai.unpublish()
+        self.ai.save()
+        VIEW = self.p("VIEW")
+        self.assertFalse(self.anon_user.has_perm(VIEW, self.discussion_post))
+        self.assertFalse(self.participant.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.moderator.has_perm(VIEW, self.discussion_post))
+        self.assertFalse(self.discusser.has_perm(VIEW, self.discussion_post))
+        self.assertFalse(self.discusser_author.has_perm(VIEW, self.discussion_post))
+
+    def test_view_upcoming(self):
+        VIEW = self.p("VIEW")
+        self.assertFalse(self.anon_user.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.participant.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.moderator.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.discusser.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.discusser_author.has_perm(VIEW, self.discussion_post))
+
+    def test_view_public_meeting_private_ai(self):
+        self.ai.unpublish()
+        self.ai.save()
+        self.meeting.public = True
+        self.meeting.save()
+        VIEW = self.p("VIEW")
+        self.assertFalse(self.anon_user.has_perm(VIEW, self.discussion_post))
+        self.assertFalse(self.participant.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.moderator.has_perm(VIEW, self.discussion_post))
+        self.assertFalse(self.discusser.has_perm(VIEW, self.discussion_post))
+        self.assertFalse(self.discusser_author.has_perm(VIEW, self.discussion_post))
+
+    def test_view_public_meeting(self):
+        self.meeting.public = True
+        self.meeting.save()
+        VIEW = self.p("VIEW")
+        self.assertTrue(self.anon_user.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.participant.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.moderator.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.discusser.has_perm(VIEW, self.discussion_post))
+        self.assertTrue(self.discusser_author.has_perm(VIEW, self.discussion_post))
+
+    def test_add(self):
+        ADD = self.p("ADD")
+        self.assertFalse(self.anon_user.has_perm(ADD, self.ai))
+        self.assertFalse(self.participant.has_perm(ADD, self.ai))
+        self.assertTrue(self.moderator.has_perm(ADD, self.ai))
+        self.assertTrue(self.discusser.has_perm(ADD, self.ai))
+
+    def test_add_with_block(self):
+        self.ai.block_discussion = True
+        self.ai.save()
+        ADD = self.p("ADD")
+        self.assertFalse(self.anon_user.has_perm(ADD, self.ai))
+        self.assertFalse(self.participant.has_perm(ADD, self.ai))
+        self.assertTrue(self.moderator.has_perm(ADD, self.ai))
+        self.assertFalse(self.discusser.has_perm(ADD, self.ai))
+
+    def test_add_archived_meeting(self):
+        self._archive()
+        ADD = self.p("ADD")
+        self.assertFalse(self.anon_user.has_perm(ADD, self.ai))
+        self.assertFalse(self.participant.has_perm(ADD, self.ai))
+        self.assertFalse(self.moderator.has_perm(ADD, self.ai))
+        self.assertFalse(self.discusser.has_perm(ADD, self.ai))
+
+    def test_change(self):
+        CHANGE = self.p("CHANGE")
+        # Maybe we want to allow changes for authors later on...
+        self.assertFalse(self.anon_user.has_perm(CHANGE, self.discussion_post))
+        self.assertFalse(self.participant.has_perm(CHANGE, self.discussion_post))
+        self.assertTrue(self.moderator.has_perm(CHANGE, self.discussion_post))
+        self.assertFalse(self.discusser.has_perm(CHANGE, self.discussion_post))
+        self.assertFalse(self.discusser_author.has_perm(CHANGE, self.discussion_post))
+
+    def test_change_archived_meeting(self):
+        self._archive()
+        CHANGE = self.p("CHANGE")
+        self.assertFalse(self.anon_user.has_perm(CHANGE, self.discussion_post))
+        self.assertFalse(self.participant.has_perm(CHANGE, self.discussion_post))
+        self.assertFalse(self.moderator.has_perm(CHANGE, self.discussion_post))
+        self.assertFalse(self.discusser.has_perm(CHANGE, self.discussion_post))
+        self.assertFalse(self.discusser_author.has_perm(CHANGE, self.discussion_post))
+
+    def test_delete(self):
+        DELETE = self.p("DELETE")
+        self.assertFalse(self.anon_user.has_perm(DELETE, self.discussion_post))
+        self.assertFalse(self.participant.has_perm(DELETE, self.discussion_post))
+        self.assertTrue(self.moderator.has_perm(DELETE, self.discussion_post))
+        self.assertFalse(self.discusser.has_perm(DELETE, self.discussion_post))
+        self.assertTrue(self.discusser_author.has_perm(DELETE, self.discussion_post))
+
+    def test_delete_archived_meeting(self):
+        self._archive()
+        DELETE = self.p("DELETE")
+        self.assertFalse(self.anon_user.has_perm(DELETE, self.discussion_post))
+        self.assertFalse(self.participant.has_perm(DELETE, self.discussion_post))
+        self.assertFalse(self.moderator.has_perm(DELETE, self.discussion_post))
+        self.assertFalse(self.discusser.has_perm(DELETE, self.discussion_post))
+        self.assertFalse(self.discusser_author.has_perm(DELETE, self.discussion_post))
