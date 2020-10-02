6df11791e291071b590d58a53625c63143aedc76
Robin Harms Oredsson <robin@betahaus.net>
2020-10-02 11:27:15 +0200

    Simple presence check system, it might need some more work with constraints and similar, but as least good enough for now. Closes #15

---
+++ voteit/presence/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'voteit.presence.apps.PresenceConfig'
---
+++ voteit/presence/models.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Optional
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.timezone import now
+from django_fsm import FSMField, transition
+from voteit.meeting.models import Meeting
+from voteit.presence.workflows import PresenceCheckWf
+
+
+class Presence(models.Model):
+    """
+    A registered presence for a specific user.
+    It's only possible to create/delete this if the presence_check is open.
+    As an object, there's no usecase to allow change since it implies tampering with the system.
+    """
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    presence_check = models.ForeignKey("PresenceCheck", on_delete=models.CASCADE, related_name="presences")
+    created = models.DateTimeField(editable=False, auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "presence_check"],
+                name="%(app_label)s_%(class)s_unique_check_for_user",
+            )
+        ]
+
+
+class PresenceCheck(models.Model):
+    """
+    This models handles lists of users who are present.
+    Only one presence check should be open at a time.
+    It enforces a relation to the PresenceSystem.
+
+    Closing it causes it to be write protected,
+    and no related objects should be changed after that.
+
+    It should be okay to delete the whole PresenceSystem though,
+    since deleting is better than tampering :)
+    """
+    state: str = FSMField(
+        default=PresenceCheckWf.initial,
+        choices=PresenceCheckWf.choices(),
+        protected=True,
+    )
+    present_users = models.ManyToManyField(
+        User, through=Presence, related_name="presences"
+    )
+    presence_system: PresenceSystem = models.ForeignKey(
+        "PresenceSystem", on_delete=models.CASCADE, related_name="presence_checks"
+    )
+    opened: datetime = models.DateTimeField(editable=False, auto_now_add=True)
+    closed: Optional[datetime] = models.DateTimeField(
+        editable=False, null=True, blank=True
+    )
+
+    @transition(field=state, source=PresenceCheckWf.OPEN, target=PresenceCheckWf.CLOSED)
+    def close(self) -> None:
+        self.closed = now()
+
+
+class PresenceSystem(models.Model):
+    """
+    The presence system has a 1-1 relation to a meeting.
+    It acts as a container for all other objects.
+    It should be possible to safely delete it and thus
+    remove all data related to presence for the related meeting.
+
+    Most of the permission checks for this system is delegated to the related meeting,
+    so there's no separate role to run presence checks.
+    """
+    meeting: Optional[Meeting] = models.OneToOneField(
+        Meeting, on_delete=models.CASCADE, null=True
+    )
