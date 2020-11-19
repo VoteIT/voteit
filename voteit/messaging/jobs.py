96e8ac70fa6eb52339d76266df3b12779a2fe33d
Robin Harms Oredsson <robin@betahaus.net>
2020-11-19 15:34:34 +0100

    Just black formating

--- voteit/messaging/jobs.py
+++ voteit/messaging/jobs.py
@@ -19,7 +19,9 @@ def signal_websocket_connect(user_pk: int = None, consumer_name: str = ""):
     user = _get_user(user_pk)
     logger.debug("%s connected consumer %s", user_pk, consumer_name)
     update_user_status(user, channel_name=consumer_name, online=True)
-    client_connect.send(sender=None, user=user, user_pk=user_pk, consumer_name=consumer_name)
+    client_connect.send(
+        sender=None, user=user, user_pk=user_pk, consumer_name=consumer_name
+    )
 
 
 @job("default", timeout=5)
@@ -27,6 +29,17 @@ def signal_websocket_close(
     user_pk: int = None, consumer_name: str = "", close_code: int = None
 ):
     user = _get_user(user_pk)
-    logger.debug("%s disconnected consumer %s. close_code: %s", user_pk, consumer_name, close_code)
-    client_close.send(sender=None, user=user, user_pk=user_pk, consumer_name=consumer_name, close_code=close_code)
+    logger.debug(
+        "%s disconnected consumer %s. close_code: %s",
+        user_pk,
+        consumer_name,
+        close_code,
+    )
+    client_close.send(
+        sender=None,
+        user=user,
+        user_pk=user_pk,
+        consumer_name=consumer_name,
+        close_code=close_code,
+    )
     update_user_status(user, channel_name=consumer_name, online=False)
