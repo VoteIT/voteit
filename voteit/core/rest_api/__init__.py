8350fd346f2c05fb6e9563b4a472cebf26cf9225
Johan Schiff <johan@radkompaniet.se>
2020-11-20 13:46:10 +0100

    Moved user handling to dev package

--- voteit/core/rest_api/__init__.py
+++ voteit/core/rest_api/__init__.py
@@ -1,11 +1,6 @@
-from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
 from . import views
 
 
 router = DefaultRouter()
-
-
-if settings.DEBUG:
-    router.register('dev-login', views.DevLogin)
