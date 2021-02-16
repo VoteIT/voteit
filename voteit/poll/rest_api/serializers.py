0ab7fc1964c16b4f52341958ef8e649c9e6a4b1f
Johan Schiff <johan@radkompaniet.se>
2021-02-16 13:43:22 +0100

    poll starting wip

--- voteit/poll/rest_api/serializers.py
+++ voteit/poll/rest_api/serializers.py
@@ -4,6 +4,7 @@ from django.db import transaction
 from rest_framework import serializers
 
 from voteit.poll import models
+from voteit.poll.abcs import PollMethod
 from voteit.poll.utils import get_poll_method_registry
 from voteit.proposal.workflows import ProposalWf
 
@@ -42,24 +43,35 @@ class PollCreateSerializer(serializers.ModelSerializer):
     _proposals: Sequence
     proposal_pks = serializers.CharField(max_length=100, write_only=True)
     start = serializers.BooleanField(write_only=True)
+    settings = serializers.JSONField(allow_null=True, write_only=True)
 
     def validate(self, attrs):
         """ Get proposal set and make sure they're all published. """
         agenda_item = attrs.get("agenda_item")
         proposal_pks = attrs.get("proposal_pks").split(",")
         method_name = attrs.get("method_name")
+        settings = attrs.get("settings")
         self._proposals = agenda_item.proposals.filter(
             pk__in=proposal_pks, state=ProposalWf.PUBLISHED
         )
         if len(self._proposals) != len(proposal_pks):
-            raise serializers.ValidationError(
-                "Proposals must be published on Agenda Item"
-            )
+            raise serializers.ValidationError({
+                "proposal_pks": "Proposals must be published on Agenda Item"
+            })
         reg = get_poll_method_registry()
-        if method_name not in reg:
-            raise serializers.ValidationError(
-                f"{method_name} is not a valid poll method. {repr(list(reg.keys()))}"
-            )
+        method: PollMethod = reg.get(method_name)
+        if method is None:
+            raise serializers.ValidationError({
+                "method_name": f"{method_name} is not a valid poll method. {repr(list(reg.keys()))}",
+            })
+        if settings is not None:
+            # TODO Better
+            try:
+                method.settings_schema(**settings)
+            except ValueError:
+                raise serializers.ValidationError({
+                    "settings": "Invalid settings"
+                })
         return super().validate(attrs)
 
     def create(self, validated_data):
@@ -78,6 +90,6 @@ class PollCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Poll
-        fields = "pk", "agenda_item", "method_name", "proposal_pks", "start"
+        fields = "pk", "agenda_item", "method_name", "proposal_pks", "start", "settings"
         extra_kwargs = {"agenda_item": {"required": True}}
         read_only_fields = "pk",
