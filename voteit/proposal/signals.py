96e9525ead74bb40b3e27511c24cf00734e17eb3
Robin Harms Oredsson <robin@betahaus.net>
2021-02-22 14:39:44 +0100

    Transmit proposals via the meeting instead

--- voteit/proposal/signals.py
+++ voteit/proposal/signals.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
-from voteit.agenda.channels import AgendaItemChannel
-from voteit.agenda.models import AgendaItem
+from voteit.meeting.channels import ParticipantsChannel
+from voteit.meeting.channels import ModeratorsChannel
 from voteit.messaging.messages.app_state import AppState
 from voteit.messaging.signals import channel_subscribed
 from voteit.proposal.messages import (
@@ -14,33 +16,57 @@ from voteit.proposal.messages import (
 )
 from voteit.proposal.models import Proposal
 from voteit.proposal.rest_api.serializers import ProposalDetailSerializer
+from voteit.agenda.workflows import AgendaItemWf
+
+if TYPE_CHECKING:
+    from voteit.meeting.models import Meeting
 
 
-@receiver(channel_subscribed, sender=AgendaItemChannel)
-def _channel_subscribed(context: AgendaItem, app_state: AppState, **kw):
+@receiver(channel_subscribed, sender=ParticipantsChannel)
+def participants_channel_subscribed(context: Meeting, app_state: AppState, **kw):
     """ Populate app_state with current proposals """
     app_state.append_from_queryset(
-        context.get_proposals(), ProposalDetailSerializer, ProposalAdded
+        Proposal.objects.filter(agenda_item__meeting=context).exclude(
+            agenda_item__state=AgendaItemWf.PRIVATE
+        ),
+        ProposalDetailSerializer,
+        ProposalAdded,
+    )
+
+
+@receiver(channel_subscribed, sender=ModeratorsChannel)
+def moderators_channel_subscribed(context: Meeting, app_state: AppState, **kw):
+    """ Populate app_state with current proposals """
+    app_state.append_from_queryset(
+        Proposal.objects.filter(agenda_item__meeting=context),
+        ProposalDetailSerializer,
+        ProposalAdded,
     )
 
 
 @receiver(post_save, sender=Proposal)
-def proposal_updated(instance=None, created=None, **kw):
-    if instance.agenda_item is None:
+def proposal_updated(instance: Proposal = None, created=None, **kw):
+    if instance.meeting is None:
         return
-    ch = AgendaItemChannel.from_instance(instance.agenda_item)
+    moderators_ch = ModeratorsChannel.from_instance(instance.meeting)
     data = ProposalDetailSerializer(instance).data
     if created:
         msg = ProposalAdded({}, **data)
     else:
         msg = ProposalChanged({}, **data)
-    ch.publish(msg)
+    moderators_ch.publish(msg)
+    if instance.agenda_item and not instance.agenda_item.is_private:
+        participants_ch = ParticipantsChannel.from_instance(instance.meeting)
+        participants_ch.publish(msg)
 
 
 @receiver(pre_delete, sender=Proposal)
 def proposal_delete(instance=None, **kw):
-    if instance.agenda_item is None:
+    if instance.meeting is None:
         return
-    ch = AgendaItemChannel.from_instance(instance.agenda_item)
+    moderators_ch = ModeratorsChannel.from_instance(instance.meeting)
     msg = ProposalDeleted({}, pk=instance.pk)
-    ch.publish(msg)
+    moderators_ch.publish(msg)
+    if instance.agenda_item and not instance.agenda_item.is_private:
+        participants_ch = ParticipantsChannel.from_instance(instance.meeting)
+        participants_ch.publish(msg)
--- voteit/proposal/tests/test_signals.py
+++ voteit/proposal/tests/test_signals.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from django.contrib.auth import get_user_model
 from django.test import TestCase, override_settings
 from voteit.agenda.channels import AgendaItemChannel
+from voteit.meeting.channels import ParticipantsChannel, ModeratorsChannel
 from voteit.messaging.messages.channels import Subscribe
 
 User = get_user_model()
@@ -12,7 +13,7 @@ _channel_layers_setting = {
 
 
 @override_settings(CHANNEL_LAYERS=_channel_layers_setting)
-class AgendaSubscribedTests(TestCase):
+class SubscribedTests(TestCase):
     def setUp(self):
         from voteit.meeting.models import Meeting
 
@@ -23,17 +24,54 @@ class AgendaSubscribedTests(TestCase):
         self.prop1 = self.ai.proposals.create()
         self.prop2 = self.ai.proposals.create()
         self.user = User.objects.create(username="user")
-        self.meeting.add_roles(self.user, "participant")
+        self.meeting.add_roles(self.user, "moderator")
 
-    def test_app_state_sent(self):
+    def test_app_state_sent_moderators(self):
         command = Subscribe(
             {"consumer_name": "abc", "user_pk": self.user.pk},
-            pk=self.ai.pk,
-            channel_type="agenda_item",
+            pk=self.meeting.pk,
+            channel_type="moderators",
         )
         msg = command.run_job()
         pks = set([x.p["pk"] for x in msg.data.app_state if x.t == "proposal.added"])
         self.assertEqual({self.prop1.pk, self.prop2.pk}, pks)
+
+    def test_app_state_sent_private_moderators(self):
+        self.ai.unpublish()
+        self.ai.save()
+        command = Subscribe(
+            {"consumer_name": "abc", "user_pk": self.user.pk},
+            pk=self.meeting.pk,
+            channel_type="moderators",
+        )
+        msg = command.run_job()
+        pks = set([x.p["pk"] for x in msg.data.app_state if x.t == "proposal.added"])
+        self.assertEqual({self.prop1.pk, self.prop2.pk}, pks)
+
+    def test_app_state_sent_participants(self):
+        command = Subscribe(
+            {"consumer_name": "abc", "user_pk": self.user.pk},
+            pk=self.meeting.pk,
+            channel_type="participants",
+        )
+        msg = command.run_job()
+        pks = set([x.p["pk"] for x in msg.data.app_state if x.t == "proposal.added"])
+        self.assertEqual({self.prop1.pk, self.prop2.pk}, pks)
+
+    def test_app_state_sent_private_participants(self):
+        self.ai.unpublish()
+        self.ai.save()
+        command = Subscribe(
+            {"consumer_name": "abc", "user_pk": self.user.pk},
+            pk=self.meeting.pk,
+            channel_type="participants",
+        )
+        msg = command.run_job()
+        app_state = msg.data.app_state
+        if app_state is None:
+            app_state = ()
+        pks = set([x.p["pk"] for x in app_state if x.t == "proposal.added"])
+        self.assertEqual(set(), pks)
 
 
 @override_settings(CHANNEL_LAYERS=_channel_layers_setting)
@@ -43,10 +81,12 @@ class ProposalChangedTests(TestCase):
 
         self.meeting = Meeting.objects.create()
         self.ai = self.meeting.agenda_items.create()
+        self.ai.upcoming()
+        self.ai.save()
         self.prop = self.ai.proposals.create()
 
-    @patch.object(AgendaItemChannel, "publish")
-    def test_added(self, mock_publish):
+    @patch.object(ParticipantsChannel, "publish")
+    def test_added_participant(self, mock_publish):
         from voteit.proposal.messages import ProposalAdded
 
         self.assertFalse(mock_publish.called)
@@ -55,9 +95,32 @@ class ProposalChangedTests(TestCase):
         msg = mock_publish.mock_calls[0].args[0]
         self.assertIsInstance(msg, ProposalAdded)
         self.assertEqual(prop.pk, msg.data.pk)
+        self.ai.unpublish()
+        self.ai.save()
+        mock_publish.reset_mock()
+        self.ai.proposals.create()
+        self.assertFalse(mock_publish.called)
 
-    @patch.object(AgendaItemChannel, "publish")
-    def test_changed(self, mock_publish):
+    @patch.object(ModeratorsChannel, "publish")
+    def test_added_moderator(self, mock_publish):
+        from voteit.proposal.messages import ProposalAdded
+
+        self.assertFalse(mock_publish.called)
+        prop = self.ai.proposals.create()
+        self.assertTrue(mock_publish.called)
+        msg = mock_publish.mock_calls[0].args[0]
+        self.assertIsInstance(msg, ProposalAdded)
+        self.assertEqual(prop.pk, msg.data.pk)
+        self.ai.unpublish()
+        self.ai.save()
+        mock_publish.reset_mock()
+        self.ai.proposals.create()
+        self.assertTrue(mock_publish.called)
+        msg = mock_publish.mock_calls[0].args[0]
+        self.assertIsInstance(msg, ProposalAdded)
+
+    @patch.object(ParticipantsChannel, "publish")
+    def test_changed_participant(self, mock_publish):
         from voteit.proposal.messages import ProposalChanged
 
         self.assertFalse(mock_publish.called)
@@ -67,14 +130,66 @@ class ProposalChangedTests(TestCase):
         msg = mock_publish.mock_calls[0].args[0]
         self.assertIsInstance(msg, ProposalChanged)
         self.assertEqual(self.prop.pk, msg.data.pk)
+        self.ai.unpublish()
+        self.ai.save()
+        mock_publish.reset_mock()
+        self.prop.body = "World"
+        self.prop.save()
+        self.assertFalse(mock_publish.called)
 
-    @patch.object(AgendaItemChannel, "publish")
-    def test_deleted(self, mock_publish):
+    @patch.object(ModeratorsChannel, "publish")
+    def test_changed_moderator(self, mock_publish):
+        from voteit.proposal.messages import ProposalChanged
+
+        self.assertFalse(mock_publish.called)
+        self.prop.body = "Hello"
+        self.prop.save()
+        self.assertTrue(mock_publish.called)
+        msg = mock_publish.mock_calls[0].args[0]
+        self.assertIsInstance(msg, ProposalChanged)
+        self.assertEqual(self.prop.pk, msg.data.pk)
+        self.ai.unpublish()
+        self.ai.save()
+        mock_publish.reset_mock()
+        self.prop.body = "World"
+        self.prop.save()
+        self.assertTrue(mock_publish.called)
+
+    @patch.object(ParticipantsChannel, "publish")
+    def test_deleted_participants(self, mock_publish):
         from voteit.proposal.messages import ProposalDeleted
 
         self.assertFalse(mock_publish.called)
         prop_pk = self.prop.pk
         self.prop.delete()
+        self.assertTrue(mock_publish.called)
+        msg = mock_publish.mock_calls[0].args[0]
+        self.assertIsInstance(msg, ProposalDeleted)
+        self.assertEqual(prop_pk, msg.data.pk)
+        self.ai.unpublish()
+        self.ai.save()
+        prop = self.ai.proposals.create()
+        mock_publish.reset_mock()
+        prop.delete()
+        self.assertFalse(mock_publish.called)
+
+    @patch.object(ModeratorsChannel, "publish")
+    def test_deleted_moderators(self, mock_publish):
+        from voteit.proposal.messages import ProposalDeleted
+
+        self.assertFalse(mock_publish.called)
+        prop_pk = self.prop.pk
+        self.prop.delete()
+        self.assertTrue(mock_publish.called)
+        msg = mock_publish.mock_calls[0].args[0]
+        self.assertIsInstance(msg, ProposalDeleted)
+        self.assertEqual(prop_pk, msg.data.pk)
+        self.ai.unpublish()
+        self.ai.save()
+        prop = self.ai.proposals.create()
+        prop_pk = prop.pk
+        mock_publish.reset_mock()
+        prop.delete()
         self.assertTrue(mock_publish.called)
         msg = mock_publish.mock_calls[0].args[0]
         self.assertIsInstance(msg, ProposalDeleted)
