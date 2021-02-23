1e7192b5901c6259a1a64ffa0fc3d24597517f51
Robin Harms Oredsson <robin@betahaus.net>
2021-02-23 11:35:08 +0100

    Transition mixin should be here for now too

--- voteit/agenda/rest_api/views.py
+++ voteit/agenda/rest_api/views.py
@@ -2,11 +2,11 @@ from rest_framework.viewsets import GenericViewSet
 
 from voteit.agenda.models import AgendaItem
 from voteit.agenda.rest_api import serializers
-from voteit.core.rest_api.mixins import CreateModelPermissionsMixin
+from voteit.core.rest_api.mixins import CreateModelPermissionsMixin, TransitionsMixin
 from voteit.meeting.models import Meeting
 
 
-class AgendaViewSet(CreateModelPermissionsMixin, GenericViewSet):
+class AgendaViewSet(CreateModelPermissionsMixin, TransitionsMixin, GenericViewSet):
     serializer_class = serializers.AgendaItemSerializer
     serializer_classes = {
         "list": serializers.AgendaListSerializer,
