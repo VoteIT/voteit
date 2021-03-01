dbc8f165c38c3fee69c94f463af80cefecb871c9
Robin Harms Oredsson <robin@betahaus.net>
2021-03-01 13:57:43 +0100

    Serializer for things that produce pydantic data

--- voteit/core/rest_api/serializers.py
+++ voteit/core/rest_api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.core.serializers.json import DjangoJSONEncoder
+from pydantic.main import BaseModel
 from rest_framework import serializers
-
+from rest_framework.fields import JSONField
 
 UserModel = get_user_model()
 
@@ -42,3 +44,59 @@ class TransitionSerializer(serializers.Serializer):
 
     class Meta:
         fields = ("transition",)
+
+
+class PydanticFieldSerializer(JSONField):
+    """Handles Pydantic representations and changes them to a rest friendly format.
+    It doesn't force pydantic in any way, if it's not a pydantic model it's handled as JSON.
+    Note that it has no knowledge of what schema it should expect.
+
+    The encoder/decoder doesn't use pydantic either
+
+    >>> from datetime import datetime
+
+    >>> class Greeting(BaseModel):
+    ...     msg: str = "Hello"
+    ...     timestamp: datetime = datetime(year=1999, month=12, day=24)
+    ...
+
+    Any other object just passes through since this is not really a validator
+    >>> field = PydanticFieldSerializer()
+    >>> field.to_internal_value({"hello": 1})
+    {'hello': 1}
+
+    JSON will be handled via pydantic instead
+    >>> result = field.to_internal_value(Greeting(msg="hello"))
+    >>> result["msg"]
+    'hello'
+    >>> result["timestamp"]
+    datetime.datetime(1999, 12, 24, 0, 0)
+
+    Empty should be okay too
+    >>> field.to_internal_value(None)
+
+    Same goes for the other way around - a dict or pydantic model loaded from model field
+    >>> field.to_representation({"hi": "there"})
+    {'hi': 'there'}
+
+    >>> result = field.to_representation(Greeting(msg="hello"))
+    >>> result["msg"]
+    'hello'
+    >>> result["timestamp"]
+    datetime.datetime(1999, 12, 24, 0, 0)
+    """
+
+    def __init__(self, *args, **kwargs):
+        """ Defualt to DjangoJSONEncoder since it handles more formats"""
+        kwargs.setdefault("encoder", DjangoJSONEncoder)
+        super().__init__(*args, **kwargs)
+
+    def to_internal_value(self, data):
+        if isinstance(data, BaseModel):
+            data = data.dict()
+        return super().to_internal_value(data)
+
+    def to_representation(self, value):
+        if isinstance(value, BaseModel):
+            value = value.dict()
+        return super().to_representation(value)
