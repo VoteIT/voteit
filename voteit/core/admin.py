3ab53a86312ac14773678f647d76375210999c86
Robin Harms Oredsson <robin@betahaus.net>
2021-04-12 14:48:38 +0200

    Added org to user in admin

--- voteit/core/admin.py
+++ voteit/core/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.utils.translation import gettext_lazy as _
+
 from voteit.core.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    fieldsets = list(DefaultUserAdmin.fieldsets)
+    fieldsets.insert(
+        1,
+        (
+            _("Organisation"),
+            {"fields": ("organisation",)},
+        ),
+    )
