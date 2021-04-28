ee560ec0d6924847a2353bd5fefad7b2428222bf
Robin Harms Oredsson <robin@betahaus.net>
2021-04-28 13:07:57 +0200

    Non-working API browser stub

--- voteit/messaging/urls.py
+++ voteit/messaging/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("echo/", views.echo, name="echo"),
+    path("msg-browser/", views.message_list),
+    path("msg-browser/<str:msg_type>/<str:name>", views.message_show),
 ]
