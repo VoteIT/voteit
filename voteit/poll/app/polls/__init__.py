d8df5b4c7343e8c00322e30edabd00ab4585a6aa
Robin Harms Oredsson <robin@betahaus.net>
2021-11-03 09:33:33 +0100

    Only cosmetic

--- voteit/poll/app/polls/__init__.py
+++ voteit/poll/app/polls/__init__.py
@@ -1,5 +1,6 @@
 from voteit.poll.app.polls.combined_simple import CombinedSimple
-from voteit.poll.app.polls.simple import Simple
+from voteit.poll.app.polls.irv import IRV
+from voteit.poll.app.polls.schulze import RepeatedSchulze
+from voteit.poll.app.polls.schulze import Schulze
 from voteit.poll.app.polls.scottish_stv import ScottishSTV
-from voteit.poll.app.polls.schulze import Schulze, RepeatedSchulze
-from .irv import IRV
+from voteit.poll.app.polls.simple import Simple
