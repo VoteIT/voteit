9306293e36c14bbb1344207c57d92f20188d4648
Robin Harms Oredsson <robin@betahaus.net>
2022-02-24 11:34:49 +0100

    Nicer admin for poll related things

--- voteit/poll/admin.py
+++ voteit/poll/admin.py
@@ -3,11 +3,44 @@ from fsm_admin.mixins import FSMTransitionMixin
 
 from voteit.poll.models import ElectoralRegister
 from voteit.poll.models import Poll
+from voteit.poll.models import VoterWeight
+
+
+class VoterWeightItemInline(admin.TabularInline):
+    model = VoterWeight
+    fields = "user", "weight"
+    extra = 0
+    autocomplete_fields = ("user",)
 
 
 @admin.register(ElectoralRegister)
 class ERAdmin(admin.ModelAdmin):
-    pass
+    list_display = "__str__", "meeting", "voters_count"
+    list_filter = ("meeting__organisation", "meeting")
+
+    def voters_count(self, er: ElectoralRegister) -> int:
+        return er.voters.count()
+
+    voters_count.short_description = "Voters"
+
+    inlines = (VoterWeightItemInline,)
+
+
+@admin.register(VoterWeight)
+class VoterWeightAdmin(admin.ModelAdmin):
+    list_display = "__str__", "meeting_title", "user", "weight"
+    list_filter = ("register__meeting__organisation", "register__meeting", "user")
+    search_fields = (
+        "register__meeting__title",
+        "user__userid",
+        "user__first_name",
+        "user__last_name",
+    )
+
+    def meeting_title(self, obj: VoterWeight):
+        return obj.register.meeting.title
+
+    meeting_title.short_description = "Meeting"
 
 
 @admin.register(Poll)
