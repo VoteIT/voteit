b035e9bc9bc433e505ba9b8d7523f5beb5dd082a
Robin Harms Oredsson <robin@betahaus.net>
2022-04-18 11:41:15 +0200

    Changed from deprecated field

--- voteit/bug_reports/models.py
+++ voteit/bug_reports/models.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
+from datetime import datetime
+
 from django.contrib.auth import get_user_model
-from django.contrib.postgres.fields import ArrayField, JSONField
+from django.contrib.postgres.fields import ArrayField
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django_fsm import FSMField, transition
 
@@ -14,34 +17,29 @@ User = get_user_model()
 
 
 class BugReport(MeetingContext):
-    meeting = models.ForeignKey(
-        Meeting, models.CASCADE
-    )
-    user = models.ForeignKey(
-        User, models.CASCADE
-    )
-    user_roles: list[str] = ArrayField(
-        models.CharField(max_length=20, default=tuple)
-    )
-    user_platform: dict = JSONField()            # navigator.userAgent, etc
+    meeting = models.ForeignKey(Meeting, models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
+    user_roles: list[str] = ArrayField(models.CharField(max_length=20, default=tuple))
+    user_platform: dict = models.JSONField(
+        encoder=DjangoJSONEncoder
+    )  # navigator.userAgent, etc
     function: str = models.CharField(max_length=20, null=True, blank=True)
     description: str = models.TextField()
     state: str = FSMField(default=BugReportWf.initial, choices=BugReportWf.choices())
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created: datetime = models.DateTimeField(auto_now_add=True)
+    updated: datetime = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = "Bug report"
         verbose_name_plural = "Bug reports"
-        ordering = "-created",
+        ordering = ("-created",)
 
     def __str__(self):
-        return f'Report by {self.user} from {self.user.organisation}'
+        return f"Report by {self.user} from {self.user.organisation}"
 
     @transition(
         field=state,
-        source='+',
+        source="+",
         target=BugReportWf.UNRESOLVED,
     )
     def back_to_unresolved(self):
@@ -49,7 +47,7 @@ class BugReport(MeetingContext):
 
     @transition(
         field=state,
-        source='+',
+        source="+",
         target=BugReportWf.IGNORED,
     )
     def ignore(self):
@@ -57,7 +55,7 @@ class BugReport(MeetingContext):
 
     @transition(
         field=state,
-        source='+',
+        source="+",
         target=BugReportWf.RESOLVED,
     )
     def resolve(self):
