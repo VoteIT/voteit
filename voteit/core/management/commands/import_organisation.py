3381c0dae3c7b923cba74e49f64de95a88861fbf
Robin Harms Oredsson <robin@betahaus.net>
2022-06-09 15:41:43 +0200

    Vote mappings weren't updated

--- voteit/core/management/commands/import_organisation.py
+++ voteit/core/management/commands/import_organisation.py
@@ -7,7 +7,26 @@ from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 from django.db import DEFAULT_DB_ALIAS
 from django.db import transaction
+from django.db.models import Model
 from dolly.core import Importer
+
+from voteit.organisation.models import Organisation
+from voteit.poll.app.polls.combined_simple import CombinedSimplePollResult
+from voteit.poll.app.polls.combined_simple import CombinedSimpleVoteSchema
+from voteit.poll.app.polls.dutt import DuttResultSchema
+from voteit.poll.app.polls.dutt import DuttVoteSchema
+from voteit.poll.app.polls.majority import MajorityPollResult
+from voteit.poll.app.polls.majority import MajorityVoteSchema
+from voteit.poll.app.polls.schulze import RepeatedSchulzeResult
+from voteit.poll.app.polls.schulze import SchulzePollResult
+from voteit.poll.app.polls.schulze import SchulzeVoteSchema
+from voteit.poll.app.polls.scottish_stv import STVResultSchema
+from voteit.poll.models import Poll
+from voteit.poll.models import Vote
+from voteit.poll.schemas import RankingSchema
+from voteit.proposal.models import Proposal
+from voteit.speaker.models import SpeakerList
+from voteit.speaker.models import SpeakerListSystem
 
 
 class Command(BaseCommand):
@@ -61,18 +80,14 @@ class Command(BaseCommand):
         # There are some reverse dependencies here, but they're for attributes we don't really need to care about
         # so we'll simply ignore them to solve the conflict.
         # This will destroy data, but active list or speaker should never be imported anyway.
-        from voteit.speaker.models import SpeakerListSystem
-        from voteit.speaker.models import SpeakerList
-        from voteit.organisation.models import Organisation
-
         importer.add_clear_attrs(SpeakerListSystem, "active_list")
         importer.add_clear_attrs(SpeakerList, "current")
         importer.add_clear_attrs(Organisation, "author", "last_modified_by")
-
+        importer.add_pre_commit(update_all_poll_results)
+        importer.add_pre_commit(update_all_votes)
+        importer.add_pre_commit(verify_json_attrs)
         if merge_org:
-            from voteit.organisation.models import Organisation
-
-            assert len(importer.data[Organisation]) == 1
+            assert len(importer.data.get(Organisation, [])) == 1
             deserialized_org = next(iter(importer.data[Organisation]))
             org = Organisation.objects.get(pk=merge_org)
             importer.replace_deserialized_object(deserialized_org, org)
@@ -101,3 +116,159 @@ class Command(BaseCommand):
             traceback.print_exc()
         else:
             print("DONE!")
+
+
+def verify_json_attrs(importer):
+    for deserialized in importer.data.get(Poll, ()):
+        for attr in ["settings", "result"]:
+            verify_schema_attr(deserialized.object, attr)
+    for deserialized in importer.data.get(Vote, ()):
+        verify_schema_attr(deserialized.object, "vote")
+    for deserialized in importer.data.get(SpeakerListSystem, ()):
+        verify_schema_attr(deserialized.object, "settings")
+
+
+def verify_schema_attr(obj, attr):
+    """
+    Touch attributes that will validate stored json data.
+    """
+    try:
+        getattr(obj, attr)
+    except ValueError as exc:
+        print(f"{obj} caused error. obj PK: {obj.pk} - attr: {attr}")
+        raise exc
+
+
+def remap_schulze_round(importer, result: dict):
+    from voteit.proposal.models import Proposal
+
+    new_pairs = []
+    for pair in result.pairs:
+        # [ [1,2], v]
+        remapped = []
+        for x in pair[0]:
+            remapped.append(importer.get_remap_obj(Proposal, x).pk)
+        new_pairs.append([remapped, pair[1]])
+    result.pairs = new_pairs
+    result.candidates = [
+        importer.get_remap_obj(Proposal, x).pk for x in result.candidates
+    ]
+    result.winner = importer.get_remap_obj(Proposal, result.winner).pk
+    strong_new_pairs = []
+    for pair in result.strong_pairs:
+        # [ [1,2], v]
+        remapped = []
+        for x in pair[0]:
+            remapped.append(importer.get_remap_obj(Proposal, x).pk)
+        strong_new_pairs.append([remapped, pair[1]])
+    result.strong_pairs = strong_new_pairs
+    if result.tied_winners:
+        result.tied_winners = [
+            importer.get_remap_obj(Proposal, x).pk for x in result.tied_winners
+        ]
+
+
+def update_all_poll_results(importer: Importer):
+    """
+    Polls and proposals have relations to each other but we can't assume proposals are processed first.
+    """
+
+    for deserialized in importer.data.get(Poll, ()):
+        if not deserialized.object.result_data:
+            continue
+        method_name = deserialized.object.method_name
+        result = deserialized.object.result
+        result.denied = [importer.get_remap_obj(Proposal, x).pk for x in result.denied]
+        result.approved = [
+            importer.get_remap_obj(Proposal, x).pk for x in result.approved
+        ]
+        if method_name == "combined_simple":
+            result: CombinedSimplePollResult
+            reformatted = {}
+            for (k, v) in result.results.items():
+                reformatted[importer.get_remap_obj(Proposal, k).pk] = v
+            result.results = reformatted
+        elif method_name == "majority":
+            result: MajorityPollResult
+            for prop_result in result.results:
+                prop_result.proposal = importer.get_remap_obj(
+                    Proposal, prop_result.proposal
+                ).pk
+        elif method_name == "dutt":
+            result: DuttResultSchema
+            for prop_result in result.results:
+                prop_result.proposal = importer.get_remap_obj(
+                    Proposal, prop_result.proposal
+                ).pk
+        elif method_name == "schulze":
+            result: SchulzePollResult
+            remap_schulze_round(importer, result)
+
+        elif method_name == "repeated_schulze":
+            result: RepeatedSchulzeResult
+            result.candidates = [
+                importer.get_remap_obj(Proposal, x).pk for x in result.candidates
+            ]
+            for res_round in result.rounds:
+                remap_schulze_round(importer, res_round)
+        elif method_name == "scottish_stv":
+            result: STVResultSchema
+            for stv_round in result.rounds:
+                stv_round.selected = [
+                    importer.get_remap_obj(Proposal, x).pk for x in stv_round.selected
+                ]
+                stv_round.vote_count = [
+                    (importer.get_remap_obj(Proposal, k).pk, v)
+                    for k, v in stv_round.vote_count
+                ]
+        else:
+            raise ValueError(
+                f"Need to handle remapping of results for method {method_name}"
+            )
+        deserialized.object.result = result.dict()
+        Model.save_base(deserialized.object, raw=True)
+
+
+def update_all_votes(importer: Importer):
+    """
+    Votes relate to proposals that will have changed PK. The json-data in votes must be changed too.
+    """
+    for deserialized in importer.data.get(Vote, []):
+        vote: Vote = deserialized.object
+        if vote.abstain:
+            continue
+        method_name = vote.poll.method_name
+        vote_data = vote.vote
+        if method_name == "combined_simple":
+            vote_data: CombinedSimpleVoteSchema
+            for attr in ("yes", "no", "abstain"):
+                new_pks = [
+                    importer.get_remap_obj(Proposal, pk).pk
+                    for pk in getattr(vote_data, attr)
+                ]
+                setattr(vote_data, attr, new_pks)
+        elif method_name == "majority":
+            vote_data: MajorityVoteSchema
+            vote_data.choice = importer.get_remap_obj(Proposal, vote_data.choice).pk
+        elif method_name == "dutt":
+            vote_data: DuttVoteSchema
+            vote_data.choices = [
+                importer.get_remap_obj(Proposal, pk).pk for pk in vote_data.choices
+            ]
+        elif method_name in ("schulze", "repeated_schulze"):
+            vote_data: SchulzeVoteSchema
+            vote_data.ranking = [
+                (importer.get_remap_obj(Proposal, pk).pk, rank)
+                for (pk, rank) in vote_data.ranking
+            ]
+        elif method_name in ("irv", "scottish_stv"):
+            vote_data: RankingSchema
+            vote_data.choices = [
+                importer.get_remap_obj(Proposal, pk).pk for pk in vote_data.choices
+            ]
+        else:
+            raise ValueError(
+                f"Need to handle remapping of vote for method {method_name}"
+            )
+        vote.vote = vote_data
+        Model.save_base(vote, raw=True)
