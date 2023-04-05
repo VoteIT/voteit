2f375b0784eeb9574e7910f22581495bb29228ec
Robin Harms Oredsson <robin@betahaus.net>
2023-04-05 17:33:15 +0200

    Send frontend version. Closes #133

--- voteit/core/signals.py
+++ voteit/core/signals.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+from os import getenv
 from typing import TYPE_CHECKING
 
 from django.db.models.signals import class_prepared
 from django.dispatch import Signal
 from django.dispatch import receiver
 
+from envelope.signals import client_connect
+from envelope.utils import websocket_send
 from voteit.core import models_to_register
 
 if TYPE_CHECKING:
@@ -24,3 +27,13 @@ def deferred_register_model(sender: Model, **kw):
     """
 
     models_to_register.add(sender)
+
+
+@receiver(client_connect)
+def send_frontend_version(consumer_name=None, **kwargs):
+    if consumer_name:
+        if FRONTEND_VERSION := getenv("FRONTEND_VERSION"):
+            from voteit.core.messages.frontend_version import FrontendVersion
+
+            msg = FrontendVersion(version=FRONTEND_VERSION)
+            websocket_send(msg, channel_name=consumer_name, on_commit=False)
