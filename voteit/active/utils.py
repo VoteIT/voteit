10fd81c4f331be0494f51e95b9b460d105e161af
Robin Harms Oredsson <robin@betahaus.net>
2023-04-21 14:17:48 +0200

    Moved so it's easier to access when mucking about in shell

--- voteit/active/utils.py
+++ voteit/active/utils.py
@@ -1,9 +1,29 @@
+from __future__ import annotations
+from datetime import timedelta
+from typing import TYPE_CHECKING
+
+from django.utils.timezone import now
+
+from envelope.models import Connection
 from voteit.active.components import ActiveUsersComponent
 from voteit.core.workflows import EnabledWf
 from voteit.meeting.models import Meeting
+
+if TYPE_CHECKING:
+    from django.db.models import QuerySet
+    from voteit.active.models import ActiveUser
 
 
 def active_enabled_for_meeting(meeting: Meeting) -> bool:
     return meeting.components.filter(
         component_name=ActiveUsersComponent.name, state=EnabledWf.ON
     ).exists()
+
+
+def get_inactive_qs(meeting: Meeting, hours: int = 2) -> QuerySet[ActiveUser]:
+    recent_enough_connections_qs = Connection.objects.filter(
+        last_action__gt=now() - timedelta(hours=hours),
+    )
+    return meeting.active_users.exclude(
+        user_id__in=recent_enough_connections_qs.values_list("user_id")
+    )
