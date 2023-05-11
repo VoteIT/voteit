319e34d176d1e819ef5aef18da305c3d8ea19a2d
Robin Harms Oredsson <robin@betahaus.net>
2023-05-11 10:27:44 +0200

    Cleanup

--- voteit/invites/utils.py
+++ voteit/invites/utils.py
@@ -40,26 +40,3 @@ def send_updated_invites(
     for data in serializer.data:
         msg = MeetingInviteChanged(data=data)
         ch.sync_publish(msg)
-
-
-# def create_dispatch_and_schedule_invites(
-#     created_by: User = None, **kwargs
-# ) -> InviteDispatch:
-#     from voteit.invites.messages import SendInvitesSchema
-#
-#     send_data = SendInvitesSchema(**kwargs)
-#     meeting: Meeting = Meeting.objects.get(pk=send_data.meeting)
-#     send_exclude_ts = now() - timedelta(hours=send_data.resend_minimum)
-#     invites_qs = meeting.invites.filter(send_state__in=send_data.states).exclude(
-#         last_sent__gt=send_exclude_ts
-#     )
-#     with transaction.atomic(durable=True):
-#         invite_dispatch: InviteDispatch = meeting.invite_dispatches.create(
-#             subject=send_data.subject,
-#             body=send_data.body,
-#             dispatcher_name=send_data.dispatcher_name,
-#             created_by=created_by,
-#         )
-#         invite_dispatch.invites.set(invites_qs)
-#         invites_qs.update(send_state=SendWf.SCHEDULED)
-#     return invite_dispatch
