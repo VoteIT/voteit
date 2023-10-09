5d149baef6aa78d6db960925f8abad0788a88574
Robin Harms Oredsson <robin@betahaus.net>
2023-10-09 10:14:10 +0200

    Try another image + use black

--- project/settings.py
+++ project/settings.py
@@ -107,7 +107,8 @@ DATABASES = {
         "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
