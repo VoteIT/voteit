1f6a7879f93bb7d88ee0f9ac924544edfc1c172d
Robin Harms Oredsson <robin@betahaus.net>
2024-02-23 10:42:29 +0100

    Refactored to speed up checks. Validation for #317

--- voteit/core/rest_api/validators.py
+++ voteit/core/rest_api/validators.py
@@ -1,51 +1,76 @@
+from typing import TYPE_CHECKING
+
+from django.utils.translation import gettext as _
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
+
 from voteit.agenda.models import AgendaItem
 from voteit.core.rest_api.serializers import BaseModelSerializer
 from voteit.meeting.permissions import MeetingPermissions
+
+if TYPE_CHECKING:
+    from voteit.meeting.models import MeetingGroup
 
 
 class ValidateGroupAIContext:
     """
     Check that request user is a member of the group, or a moderator
-    Make sure the group is part of this meeting
+    Make sure the group is part of this meeting.
+    Also checks as_group combinations.
     This is for a ForeignKey field and only on a create operation!
     """
 
     requires_context = True
 
-    def __init__(self, group_fieldname="meeting_group"):
+    def __init__(self, group_fieldname="meeting_group", as_group_fieldname="as_group"):
         self.group_fieldname = group_fieldname
+        self.as_group_fieldname = as_group_fieldname
 
     def __call__(self, value, serializer: BaseModelSerializer):
+        """
+        Note the requirement on VoteITs BaseModelSerializer - it checks meeting groups so we don't need to do that.
+        """
         if self.group_fieldname in value:
             assert isinstance(serializer, BaseModelSerializer)
             user = serializer.get_request_user()
-            group = value[self.group_fieldname]
+            if user is None or user.is_anonymous:  # pragma: no cover
+                # This should never really happen since create will always require authenticated users
+                raise AuthenticationFailed()
+            group: MeetingGroup = value[self.group_fieldname]
             if group is not None:
                 # AI needed for both tests
                 agenda_item = value["agenda_item"]
-                if isinstance(agenda_item, int):
-                    agenda_item = AgendaItem.objects.get(pk=agenda_item)
+                assert isinstance(agenda_item, AgendaItem)
                 # Check group in meeting
-                if agenda_item.meeting is None:
+                if agenda_item.meeting is None:  # pragma: no cover
+                    # Unattached agenda items aren't allowed right now, but keep this regardless
                     raise serializers.ValidationError(
                         {
                             self.group_fieldname: "Agenda item isn't attached to a meeting"
                         }
                     )
-                if agenda_item.meeting != group.meeting:
-                    raise serializers.ValidationError(
-                        {
-                            self.group_fieldname: f"MeetingGroup doesn't exist within this meeting"
-                        }
-                    )
-                # Check group membership
-                if not group.members.filter(pk=user.pk).exists():
-                    if not user.has_perm(
-                        MeetingPermissions.MODERATE, agenda_item.meeting
-                    ):
+                # Moderator? Abort the rest of the check in that case
+                if user.has_perm(MeetingPermissions.MODERATE, agenda_item.meeting):
+                    return
+                # This op doesn't require a DB lookup so do it before group check
+                if value.get("as_group"):
+                    if not group.post_as:
                         raise serializers.ValidationError(
                             {
-                                self.group_fieldname: f"User {user.pk} is not in group {group.pk} or moderator"
+                                self.as_group_fieldname: _(
+                                    "This meeting group doesn't allow you to create posts in its name. "
+                                    "(post_as=False)"
+                                )
                             }
                         )
+                else:
+                    if not group.post_with:
+                        raise serializers.ValidationError(
+                            {
+                                self.group_fieldname: _(
+                                    "This meeting group doesn't allow you to display it together with your posts. "
+                                    "(post_with=False)"
+                                )
+                            }
+                        )
+                # Group membership check should be done via BaseModelSerializer
