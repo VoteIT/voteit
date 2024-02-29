9fa4df8b3942b808b8a34d7837cf5e9e7edc49a0
Robin Harms Oredsson <robin@betahaus.net>
2024-02-29 15:08:03 +0100

    Mark text messages and response

--- voteit/room/messages.py
+++ voteit/room/messages.py
@@ -1,7 +1,16 @@
+from pydantic import BaseModel
+from pydantic import validator
+
+from envelope.core.message import ContextAction
+from envelope.core.message import Message
 from voteit.messaging.base import BaseObjectAdded
 from voteit.messaging.base import BaseObjectChanged
 from voteit.messaging.base import BaseObjectDeleted
+from voteit.messaging.decorators import incoming
 from voteit.messaging.decorators import outgoing
+from voteit.room.channels import RoomChannel
+from voteit.room.models import Room
+from voteit.room.permissions import RoomPermissions
 
 
 @outgoing
@@ -22,3 +31,71 @@ class RoomHighlighted(BaseObjectChanged):
 @outgoing
 class RoomDeleted(BaseObjectDeleted):
     name = "room.deleted"
+
+
+class RoomMarkTextSchema(BaseModel):
+    """
+    >>> S = RoomMarkTextSchema
+    >>> S(room=1).dict(exclude_unset=True)
+    {'room': 1}
+
+    >>> S(proposal=1, start=1, end=2, room=1).dict(exclude_unset=True)
+    {'start': 1, 'end': 2, 'proposal': 1}
+
+    >>> S(start=1, end=2, room=1).dict(exclude_unset=True)
+    Traceback (most recent call last):
+    ...
+    pydantic.error_wrappers.ValidationError:
+
+    >>> S(start=1, proposal=1, room=1).dict(exclude_unset=True)
+    Traceback (most recent call last):
+    ...
+    pydantic.error_wrappers.ValidationError:
+    """
+
+    room: int
+    start: int | None = None
+    end: int | None = None
+    proposal: int | None = None
+
+    @validator("end", always=True)
+    def validate_start_end(cls, v: int | None, values: dict):
+        start = values.get("start")
+        if bool(v) != bool(start):
+            raise ValueError("Both start and end must be a numer or None")
+        if isinstance(start, int) and not start < v:
+            raise ValueError("end must be higher than start")
+        return v
+
+    @validator("proposal", always=True)
+    def validate_proposal(cls, v: int | None, values: dict):
+        start = values.get("start")
+        if bool(v) != bool(start):
+            raise ValueError("proposal must be the same type as start and end")
+        return v
+
+
+@incoming
+class RoomMarkText(ContextAction):
+    name = "room.mark_text"
+    permission = RoomPermissions.HANDLE
+    model = Room
+    schema = RoomMarkTextSchema
+    data: RoomMarkTextSchema
+
+    def run_job(self):
+        # We won't raise errors or send responses for these kind of messages
+        # since that will only be annoying for moderators
+        if self.allowed():
+            ch = RoomChannel.from_instance(self.context)
+            msg = RoomMarked.from_message(self, **self.data.dict())
+            ch.sync_publish(msg, on_commit=False)
+            # No effect but might be nice for testing
+            return msg
+
+
+@outgoing
+class RoomMarked(Message):
+    name = "room.marked"
+    schema = RoomMarkTextSchema
+    data: RoomMarkTextSchema
