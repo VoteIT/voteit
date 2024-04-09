90a93c4a78998060d955b7f16099a4c0a95d0cf7
Robin Harms Oredsson <robin@betahaus.net>
2024-04-09 21:46:38 +0200

    Reverted

--- project/settings.py
+++ project/settings.py
@@ -43,7 +43,7 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-LANGUAGE_CODE = "sv-se"
+LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Europe/Stockholm"
 USE_I18N = True
 USE_L10N = True
