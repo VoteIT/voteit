fae160005594950b7b44d110b714184f929d1ae4
Robin Harms Oredsson <robin@betahaus.net>
2026-03-24 08:47:29 +0100

    Cleanup presence check

--- voteit/presence/apps.py
+++ voteit/presence/apps.py
@@ -7,9 +7,5 @@ class PresenceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
-        from voteit.presence import components
-        from voteit.presence import rules
-        from voteit.presence import channels
-        from voteit.presence import messages
-        from voteit.presence import signals
-        from .rest_api import views
+        from voteit.presence import components  # noqa
+        from voteit.presence import rules  # noqa
--- voteit/presence/tests/test_docs.py
+++
@@ -1,7 +0,0 @@
-from voteit import presence
-from voteit.core.testing import load_doctests
-
-
-def load_tests(loader, tests, pattern):
-    load_doctests(tests, presence)
-    return tests
