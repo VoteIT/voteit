06ace30fb897c89598578701ea79918367cfc40c
Robin Harms Oredsson <robin@betahaus.net>
2026-04-29 09:20:29 +0200

    Syntax fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-#find /data  -not -user voteit -exec chown voteit:voteit {} \+
 
 if [[ "$1" == "run" ]]; then
   echo "Starting Daphne"
@@ -8,14 +7,15 @@ if [[ "$1" == "run" ]]; then
     ./wait-for-it.sh redis:6379 -s -- \
     daphne --access-log - -p 8000 -b 0.0.0.0 --ping-interval 10 --proxy-headers project.routing:application
 elif [[ "$1" == "worker" ]]; then
-  if [[ ! $2 ]]; then
-    $2 = default
+  QUEUES=("${@:2}")
+  if [[ ${#QUEUES[@]} -eq 0 ]]; then
+    QUEUES=("default")
     echo "Queue set to default"
   fi
-  echo "Starting worker with scheduler and queue(s) " "${@:2}"
+  echo "Starting worker with scheduler and queue(s): ${QUEUES[*]}"
   exec ./wait-for-it.sh db:5432 -s -- \
     ./wait-for-it.sh redis:6379 -s -- \
-    ./manage.py rqworker --with-scheduler "${@:2}"
+    ./manage.py rqworker --with-scheduler "${QUEUES[@]}"
 elif [[ "$1" == "shell" ]]; then
   exec ./manage.py shell "${@:2}"
 elif [[ "$1" == "manage" ]]; then
