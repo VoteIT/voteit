7eb7698ac1b08de890c66b21542142fe0cdaee98
Robin Harms Oredsson <robin@betahaus.net>
2026-06-01 11:40:44 +0200

    Cleanup

--- voteit/meeting/messages.py
+++ voteit/meeting/messages.py
@@ -1,5 +1,3 @@
-from pydantic import BaseModel
-
 from voteit.messaging.base import BaseObjectAdded
 from voteit.messaging.base import BaseObjectChanged
 from voteit.messaging.base import BaseObjectDeleted
@@ -59,39 +57,3 @@ class GroupMembershipChanged(BaseObjectChanged):
 @outgoing
 class GroupMembershipDeleted(BaseObjectDeleted):
     name = "group_membership.deleted"
-
-
-class CopyMeetingSchema(BaseModel):
-    meeting: int
-    # Settings etc later on
-
-
-# @incoming
-# class CopyMeeting(ContextAction):
-#     name = "meeting.create_copy"
-#     permission = MeetingPermissions.VIEW
-#     schema = CopyMeetingSchema
-#     data: CopyMeetingSchema
-#     model = Meeting
-#     context_schema_attr = "meeting"
-#
-#     def run_job(self):
-#         # Read meeting
-#         self.assert_perm()
-#         organisation = self.user.organisation
-#         assert organisation
-#         # Double permission check here, since add is needed too
-#         if not self.user.has_perm(MeetingPermissions.ADD, organisation):
-#             raise UnauthorizedError.from_message(
-#                 self,
-#                 model=organisation.__class__,
-#                 key="pk",
-#                 value=organisation.pk,
-#                 permission=MeetingPermissions.ADD,
-#             )
-#         meeting: Meeting = self.context
-#         update = Status.from_message(self)
-#         websocket_send(update, state=update.RUNNING, on_commit=False)
-#         with set_actor(self.user):
-#             clone_meeting(meeting, user=self.user)
-#         websocket_send(update, state=update.SUCCESS)
