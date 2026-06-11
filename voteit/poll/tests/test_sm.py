9814bd6335fbabcf75ee33fd7ff9a8fae1c119e2
Robin Harms Oredsson <robin@betahaus.net>
2026-06-11 13:33:25 +0200

    Transition tests/perms

--- voteit/poll/tests/test_sm.py
+++ voteit/poll/tests/test_sm.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.exceptions import PermissionDenied
 
 from voteit.agenda.models import AgendaItem
 from voteit.meeting.models import Meeting
 from voteit.meeting.roles import ROLE_POTENTIAL_VOTER
+from voteit.poll.exceptions import InvalidPollSettings
 from voteit.poll.models import ElectoralRegister
 from voteit.poll.models import Poll
 from voteit.poll.statemachines import PollStateMachine
@@ -111,3 +113,45 @@ class PollSMTests(TestCase):
         self.poll.withhold_result(force=True)
         self.assertEqual("withheld", self.poll.state)
         self.assertTrue(self.poll.withheld_result)
+
+    # --- Permission tests ---
+
+    def test_make_upcoming_denied_for_non_moderator(self):
+        with self.assertRaises(PermissionDenied):
+            self.poll.upcoming(user=self.voter)
+        self.assertEqual("private", self.poll.state)
+
+    def test_make_upcoming_allowed_for_moderator(self):
+        self.poll.upcoming(user=self.moderator)
+        self.assertEqual("upcoming", self.poll.state)
+
+    def test_close_denied_for_non_moderator(self):
+        self.poll.ongoing(force=True)
+        with self.assertRaises(PermissionDenied):
+            self.poll.close(user=self.voter)
+
+    def test_close_allowed_for_moderator(self):
+        self.poll.ongoing(force=True)
+        self.poll.close(user=self.moderator)
+        self.assertNotEqual("ongoing", self.poll.state)
+
+    # --- Transition coverage ---
+
+    def test_validate_settings_blocks_make_upcoming(self):
+        poll = self.ai.polls.create(method_name="schulze", settings_data={"stars": 100})
+        with self.assertRaises(InvalidPollSettings):
+            poll.upcoming(force=True)
+
+    def test_failed_to_no_result_on_close(self):
+        poll = self.ai.polls.create(method_name="simple")
+        Poll.objects.filter(pk=poll.pk).update(state=PollStateMachine.failed.value)
+        poll.refresh_from_db()
+        poll.close(force=True)
+        self.assertEqual("no_result", poll.state)
+
+    def test_canceled_to_no_result_on_close(self):
+        self.poll.ongoing(force=True)
+        self.poll.cancel(force=True)
+        self.assertEqual("canceled", self.poll.state)
+        self.poll.close(force=True)
+        self.assertEqual("no_result", self.poll.state)
